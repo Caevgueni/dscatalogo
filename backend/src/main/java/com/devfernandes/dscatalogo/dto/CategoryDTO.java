f7120994d642631bfb1c70f2319b5bc15fd99b6c
Carlos Fernandes <itais1@hotmail.com>
2026-09-01 00:08:38 +0100

    impelmentado DTO da categoria

---
+++ backend/src/main/java/com/devfernandes/dscatalogo/dto/CategoryDTO.java
@@ -0,0 +1,37 @@
+package com.devfernandes.dscatalogo.dto;
+
+import com.devfernandes.dscatalogo.entities.Category;
+
+public class CategoryDTO {
+
+	private Long id;
+	private String name;
+
+	
+	public CategoryDTO() {
+
+	}
+
+	public CategoryDTO(Long id, String name) {
+
+		this.id = id;
+		this.name = name;
+	}
+
+	public CategoryDTO(Category entity) {
+
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	
+	
+}
--- backend/src/main/java/com/devfernandes/dscatalogo/services/CategoryService.java
+++ backend/src/main/java/com/devfernandes/dscatalogo/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.devfernandes.dscatalogo.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devfernandes.dscatalogo.dto.CategoryDTO;
 import com.devfernandes.dscatalogo.entities.Category;
 import com.devfernandes.dscatalogo.repositories.CategoryRepository;
 
@@ -18,9 +20,9 @@ public class CategoryService {
 	private CategoryRepository categoryRepository;
 	
 	@Transactional(readOnly =true)
-	public List<Category>  findAll() {
-		
-	return	categoryRepository.findAll();
+	public List<CategoryDTO>  findAll() {
+		List<Category> list = categoryRepository.findAll();
+	return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 
 }
