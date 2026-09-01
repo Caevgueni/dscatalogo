5669303fc8e36db01c697ce3ca29d0b703f16c1e
Carlos Fernandes <itais1@hotmail.com>
2026-09-01 17:36:52 +0100

    Dados de auditoria

--- backend/src/main/java/com/devfernandes/dscatalogo/entities/Category.java
+++ backend/src/main/java/com/devfernandes/dscatalogo/entities/Category.java
@@ -1,12 +1,15 @@
 package com.devfernandes.dscatalogo.entities;
 
+import java.time.Instant;
 import java.util.Objects;
 
-import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 
 @Entity
@@ -17,7 +20,13 @@ public class Category {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
 
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
+	
 	public Category() {
 	}
 
@@ -41,6 +50,22 @@ public class Category {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	
+	
+	@PrePersist
+	public void prePersiste() {
+		createdAt = Instant.now();
+	}
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
 	}
 
 	@Override
