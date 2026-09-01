c1efc365adeebcc02b379f63b442bf13da5321f1
Carlos Fernandes <itais1@hotmail.com>
2026-09-01 20:42:41 +0100

    Produto Entity, e Mapeamento JPA N-N e script SQL completo

--- backend/src/main/java/com/devfernandes/dscatalogo/entities/Product.java
+++ backend/src/main/java/com/devfernandes/dscatalogo/entities/Product.java
@@ -1,14 +1,43 @@
 package com.devfernandes.dscatalogo.entities;
 
+import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name ="tb_product")
 public class Product {
 	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Column(columnDefinition = "TEXT")
 	private String description;
 	private Double price;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant date;
+	
+	@ManyToMany       
+	@JoinTable(name ="tb_product_category",
+	joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name ="category_id"))
+	Set<Category> categories = new HashSet<>();
 	
 	public Product() {
 
@@ -16,12 +45,13 @@ public class Product {
 	}
 
 	
-	public Product(Long id, String name, String description, Double price, String imgUrl) {
+	public Product(Long id, String name, String description, Double price, String imgUrl, Instant date) {
 		this.id = id;
 		this.name = name;
 		this.description = description;
 		this.price = price;
 		this.imgUrl = imgUrl;
+		this.date = date;
 	}
 
 
@@ -72,6 +102,21 @@ public class Product {
 
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
+	}
+
+
+	public Instant getDate() {
+		return date;
+	}
+
+
+	public void setDate(Instant date) {
+		this.date = date;
+	}
+
+
+	public Set<Category> getCategories() {
+		return categories;
 	}
 
 
