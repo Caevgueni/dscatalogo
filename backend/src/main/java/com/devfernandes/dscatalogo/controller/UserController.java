d3138df4b687015d9461704dc982f2429ad0c778
Carlos Fernandes <itais1@hotmail.com>
2026-09-09 17:23:34 +0100

    UserUpdateValidator PARTE-2

--- backend/src/main/java/com/devfernandes/dscatalogo/controller/UserController.java
+++ backend/src/main/java/com/devfernandes/dscatalogo/controller/UserController.java
@@ -17,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devfernandes.dscatalogo.dto.UserDTO;
+import com.devfernandes.dscatalogo.dto.UserInsertDTO;
+import com.devfernandes.dscatalogo.dto.UserUpdateDTO;
 import com.devfernandes.dscatalogo.services.UserService;
 
 import jakarta.validation.Valid;
@@ -44,7 +46,7 @@ public class UserController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<UserDTO> insert(@Valid @RequestBody UserUpdateDTO dto){
+	public ResponseEntity<UserDTO> insert(@Valid @RequestBody UserInsertDTO dto){
 		UserDTO newDTO = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(newDTO.getId()).toUri();
@@ -52,9 +54,9 @@ public class UserController {
 	}
 	
 	@PutMapping(value ="/{id}")
-	public ResponseEntity<UserDTO> update(@PathVariable Long id,@Valid @RequestBody UserDTO dto){
-		dto = service.update(id, dto);
-		return ResponseEntity.ok().body(dto);
+	public ResponseEntity<UserDTO> update(@PathVariable Long id,@Valid @RequestBody UserUpdateDTO dto){
+	UserDTO	newDto = service.update(id, dto);
+		return ResponseEntity.ok().body(newDto);
 	}
 	
 	@DeleteMapping(value ="/{id}")
--- backend/src/main/java/com/devfernandes/dscatalogo/services/validatins/UserUpdateValidator.java
+++ backend/src/main/java/com/devfernandes/dscatalogo/services/validatins/UserUpdateValidator.java
@@ -2,33 +2,46 @@ package com.devfernandes.dscatalogo.services.validatins;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.service.invoker.RequestAttributeArgumentResolver;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.devfernandes.dscatalogo.controller.exception.FieldMessage;
 import com.devfernandes.dscatalogo.dto.UserInsertDTO;
+import com.devfernandes.dscatalogo.dto.UserUpdateDTO;
 import com.devfernandes.dscatalogo.entities.User;
 import com.devfernandes.dscatalogo.repositories.UserRepository;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserInsertDTO> {
+public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserUpdateDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
 	
 	@Autowired
 	private UserRepository userRepository;
 	
 	@Override
-	public void initialize(UserInsertValid ann) {
+	public void initialize(UserUpdateValid ann) {
 	}
 
 	@Override
-	public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+	public boolean isValid(UserUpdateDTO dto, ConstraintValidatorContext context) {
+		
+		var uriVars = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		long userId = Long.parseLong(uriVars.get("id"));
 		
 		List<FieldMessage> list = new ArrayList<>();
+		
 		//fazemos consultas paraver se o email já existe
 		User user = userRepository.findByEmail(dto.getEmail());
-		if (user != null) {
+		if (user != null && userId != user.getId()) {
 			list.add(new FieldMessage("email", "Email já existe"));
 		}
 		
