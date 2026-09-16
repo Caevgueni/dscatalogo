df1c676b57f11ed49e08b2cad07d757f5e43623e
Carlos Fernandes <itais1@hotmail.com>
2026-09-16 12:33:30 +0100

    Preparando o DSCatalog para envio de email

---
+++ backend/src/main/java/com/devfernandes/dscatalogo/services/exceptions/EmailException.java
@@ -0,0 +1,9 @@
+package com.devfernandes.dscatalogo.services.exceptions;
+
+@SuppressWarnings("serial")
+public class EmailException extends RuntimeException {
+
+	public EmailException(String msg) {
+		super(msg);
+	}
+}
