8d71bec220c62f3593317912c29a1f3ed4b33d74
Carlos Fernandes <itais1@hotmail.com>
2026-09-16 14:29:19 +0100

    verificar se o email existe

--- backend/src/main/java/com/devfernandes/dscatalogo/services/AuthService.java
+++ backend/src/main/java/com/devfernandes/dscatalogo/services/AuthService.java
@@ -1,15 +1,25 @@
 package com.devfernandes.dscatalogo.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devfernandes.dscatalogo.dto.EmailDTO;
-
-import jakarta.validation.Valid;
+import com.devfernandes.dscatalogo.entities.User;
+import com.devfernandes.dscatalogo.repositories.UserRepository;
+import com.devfernandes.dscatalogo.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class AuthService {
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	public void createRcouverToken(EmailDTO body) {
+		
+		User user = userRepository.findByEmail(body.getEmail());
+		if(user == null) {
+			throw new ResourceNotFoundException("Email nao encontrado");
+		}
 		
 	}
 
